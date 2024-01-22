9f95f29a33fcfce90175bcc8edfe3c0aa91e2f72
Makzui <144598694+Makzui@users.noreply.github.com>
2024-01-22 20:57:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.is_valid_phone(value):
+            raise ValueError
+        super().__init__(value)
+
+    @staticmethod
+    def is_valid_phone(value):
+        return len(value) == 10 and value.isdigit()
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                break
+
+    def edit_phone(self, old_phone, new_phone):
+        if not Phone.is_valid_phone(new_phone):
+            raise ValueError
+        phone_to_edit = self.find_phone(old_phone)
+        if phone_to_edit:
+            phone_to_edit.value = new_phone
+        else:
+            raise ValueError
+
+    def find_phone(self, phone):
+        return next((p for p in self.phones if p.value == phone), None)
+
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
